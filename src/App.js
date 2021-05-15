e6d27fa45329c7355f5f3f4b941f5708887c0c02
vaslava <vliavas@rogers.com>
2021-05-15 14:40:24 -0400

    Message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Broski.
+          Hello Broski.  Love this stuff!
         </p>
         <a
           className="App-link"
